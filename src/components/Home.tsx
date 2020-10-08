63914f7db550f9be238dc6136407afde96005831
guychien <guychienll@gmail.com>
2020-10-08 13:21:33 +0800

    chore: update the portfolio

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-
+import todolistImg from "../images/todolist.jpg";
+import sixtySecondGaming from "../images/sixty-second-gaming.jpg";
+import aqiIndexService from "../images/aqi-index-service.jpg";
+import tictactoe from "../images/tictactoe.jpg";
+import muses from "../images/muses.jpg";
+import canvas from "../images/canvas.jpg";
 const Wrapper = styled.div`
   min-width: 320px;
   display: flex;
@@ -26,6 +31,9 @@ const Work = styled.a<{ href: string }>`
     box-sizing: border-box;
     display: block;
     max-width: 280px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
   }
   .overlay {
     position: absolute;
@@ -36,7 +44,7 @@ const Work = styled.a<{ href: string }>`
     height: 100%;
     width: 100%;
     opacity: 0;
-    transition: 0.5s ease;
+    transition: all 0.5s ease;
     background-color: #eee;
     display: flex;
     align-items: center;
@@ -49,50 +57,48 @@ const Work = styled.a<{ href: string }>`
     .overlay {
       opacity: 0.7;
     }
+    box-shadow: 0 0 10px 2px #bbb;
   }
 `;
 
 const works = [
   {
-    link: "",
-    imgSrc: "https://c.tadst.com/gfx/600x337/full-moon.png?1",
-    name: "moon.jpg",
+    link: "https://guychienll.github.io/sixty-seconds-gaming/",
+    imgSrc: sixtySecondGaming,
+    name: "sixtySecondGaming.jpg",
+    orderId: 3,
+  },
+  {
+    link: "https://muses-6c8bb.web.app/menu",
+    imgSrc: muses,
+    name: "muses.jpg",
     orderId: 2,
   },
   {
-    link: "",
-    imgSrc:
-      "https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg",
-    name: "moon.jpg",
+    link: "https://guychienll.github.io/canvas",
+    imgSrc: canvas,
+    name: "canvas.jpg",
     orderId: 1,
-  },
-  {
-    link: "",
-    imgSrc: "https://www.w3schools.com/css/paris.jpg",
-    name: "moon.jpg",
-    orderId: 3,
   },
 ];
 const works2 = [
   {
-    link: "",
-    imgSrc: "https://c.tadst.com/gfx/600x337/full-moon.png?1",
-    name: "moon.jpg",
+    link: "https://guychienll.github.io/tictactoe/",
+    imgSrc: tictactoe,
+    name: "tictactoe.jpg",
     orderId: 1,
   },
   {
-    link: "",
-    imgSrc:
-      "https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg",
-    name: "moon.jpg",
-    orderId: 2,
+    link: "https://guychienll.github.io/aqi-index-service/",
+    imgSrc: aqiIndexService,
+    name: "aqiIndexService.jpg",
+    orderId: 3,
   },
   {
-    link: "",
-    imgSrc:
-      "https://pyxis.nymag.com/v1/imgs/a18/a96/4cab53acf59286c159442a729cd05dd95c-7-pinkmoon.rsquare.w700.jpg",
-    name: "moon.jpg",
-    orderId: 3,
+    link: "https://guychienll.github.io/todolist",
+    imgSrc: todolistImg,
+    name: "todolist.jpg",
+    orderId: 2,
   },
 ];
 
