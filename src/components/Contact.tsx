782209c86a7266cb852eb5d64cc11dfc84df0c0e
guychien <guychienll@gmail.com>
2020-10-08 16:01:11 +0800

    feat: contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Contact: React.FC = () => {
-  return <div>Contact</div>;
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLScWgfOnp2IjTfC0176qJoZZTjWOFF8Dua8WKo1VG-OvWOcFQA/viewform?embedded=true"
+      width="640"
+      height="700"
+    >
+      載入中…
+    </iframe>
+  );
 };
 export default Contact;
